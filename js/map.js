b0cc72089eafcffa88f244985981614bbbf1501a
James Barry <jbarry@pelicom.net>
2017-02-01 06:23:26 -0600

    Change markers var

--- js/map.js
+++ js/map.js
@@ -1,5 +1,6 @@
 
 var map;
+var markers;
 function initMap() {
 	var home = new google.maps.LatLng(33.050945,-87.715825);
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -7,6 +8,6 @@ function initMap() {
 	  zoom: 11
 	});
 
-	var markers = ko.observableArray([]);
+	markers = ko.observableArray([]);
 	var apiaryListInMap = ko.observableArray([]);
 }
